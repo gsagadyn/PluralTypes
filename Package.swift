b3bc2ddbf920cf279df9c360e94707b04573b1a1
Kamil Modzelewski <kammodze@gmail.com>
2021-04-30 17:27:11 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "PluralTypes",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "PluralTypes", targets: ["PluralTypes"])
+    ],
+    targets: [
+        .target(name: "PluralTypes", dependencies: [], path: "PluralTypes", resources: [.process("Assets"))
+    ]
+)
